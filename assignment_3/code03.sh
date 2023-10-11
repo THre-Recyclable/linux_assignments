792d1918ba899d0678cc3559ba4ff8153ead199a
KBD <forgive7145@gmail.com>
2023-10-11 04:28:39 -0700

    03:shell script programming

---
+++ assignment_3/code03.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# used 'bash' instead of 'sh' to use regular expression
+
+if [ "$#" -ne 2 ]; then
+    echo "Invalid input: need 2 arguments"
+    exit 1
+fi
+
+# only accept positive integers
+if ! [[ $1 =~ ^[1-9][0-9]*$ ]] || ! [[ $2 =~ ^[1-9][0-9]*$ ]]; then
+    echo "Arguments must be positive integers."
+    exit 1
+fi
+
+row=$1
+col=$2
+
+for (( i=1; i<=row; i++ )); do
+    for (( j=1; j<=col; j++ )); do
+        result=$(( i * j ))
+        printf "$i*$j=$result  "
+    done
+    echo ""
+done
